3b8e034aa490357c5427d13b310662b14775b714
Priyanka Negi <priyanka.negi@abjayon.com>
2020-06-10 16:15:23 +0530

    add about text

--- public/js/app.js
+++ public/js/app.js
@@ -24,26 +24,3 @@ weatherForm.addEventListener('submit', (e) =>{
 
 })
 
-
-// const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Boston.json?access_token=pk.eyJ1IjoicHJpeWFua2FuZWdpIiwiYSI6ImNrYjdlb3NlbDA0eXoyeHF6dW1manVqYm8ifQ.nSOQKPGccewizUpGOAi74w&limit=1';
-
-// fetch(url).then((response) => {
-//     response.json().then((data) =>{
-//         const latitude = data.features[0].center[1]
-//         const longitude= data.features[0].center[0]
-//         console.log('latitude: ' + data.features[0].center[1])
-//         console.log('longitude: '  + data.features[0].center[0])
-//         console.log('location: ' + data.features[0].place_name)
-
-//         const forecastUrl = 'http://api.weatherstack.com/forecast?access_key=c1fc839bb74945e1cdea3ae7514faba5&query='+ latitude+' ,'+ longitude + '&units=f';
-
-//         fetch(forecastUrl).then((forecastResponse) => {
-//             forecastResponse.json().then((forecastData) => {
-//                 console.log(forecastData.current.weather_descriptions[0] + '. It is currently ' + forecastData.current.temperature + ' degrees out. It feels like ' + forecastData.current.feelslike + ' degrees out.')
-                
-//             })
-//         })
-
-
-//     })
-// })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('postman-request');
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/forecast?access_key=c1fc839bb74945e1cdea3ae7514faba5&query='+ latitude+' ,'+ longitude + '&units=f';
+    const url = 'http://api.weatherstack.com/forecast?access_key=c1fc839bb74945e1cdea3ae7514faba5&query='+ latitude+' ,'+ longitude + '&units=m';
 
     request({url, json:true}, (error, {body}) => {
         if(error){
